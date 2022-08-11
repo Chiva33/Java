cd10f2cc040b65814ab6eae58630e9f8dd7ea7b7
Kirill K <francesc12008@gmail.com>
2022-08-11 19:16:11 +0300

    test

--- hw1/hw1_2.java
+++ hw1/hw1_2.java
@@ -1,12 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count = 0;
+       int insInd = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[count] = nums[i];
-                count++;
+                nums[insInd] = nums[i];
+                insInd++;
             }
         }
-        return count;
+        return insInd;
     }
 }
